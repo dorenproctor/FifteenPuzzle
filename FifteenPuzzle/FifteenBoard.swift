1d517886022e7e12c0dd7dc7bfa1ec76b8396918
Doren Proctor <doren.proctor@wsu.edu>
2018-02-18 19:57:36 -0800

    Tiles now move correctly

--- FifteenPuzzle/FifteenBoard.swift
+++ FifteenPuzzle/FifteenBoard.swift
@@ -17,33 +17,88 @@ class FifteenBoard {
     ]
     
     func scramble(numTimes n: Int) {
-        
+        var i = n
+        while (i>0) {
+            // get a random tile and slide it
+            i -= 1
+        }
     }
     func getTile(atRow r: Int, atColumn c: Int) -> Int {
-        return 1
+        return state[r][c]
     }
     func getRowAndColumn(forTile tile: Int) -> (row: Int, column: Int)?  {
-        return (1, 1)
+        for i in 0...3 {
+            for j in 0...3 {
+                if (state[i][j] == tile) {
+                    return (i, j)
+                }
+            }
+        }
+        return (-1, -1)
     }
     func isSolved() -> Bool {
-        return true
+        if (state[0] == [1, 2, 3, 4] &&
+            state[1] == [5, 6, 7, 8] &&
+            state[2] == [9, 10, 11, 12] &&
+            state[3] == [13, 14, 15, 0]) {
+            return true
+        }
+        return false
     }
     func canSlideTileUp(atRow r : Int, Column c : Int) -> Bool {
-        return true
+        if (r > 0) {
+            if (state[r-1][c] == 0) {
+                return true
+            }
+        }
+        return false
     }
     func canSlideTileDown(atRow r : Int, Column c : Int) -> Bool {
-        return true
+        if (r < 3) {
+            if (state[r+1][c] == 0) {
+                return true
+            }
+        }
+        return false
     }
     func canSlideTileLeft(atRow r : Int, Column c : Int) -> Bool {
-        return true
+        if (c > 0) {
+            if (state[r][c-1] == 0) {
+                return true
+            }
+        }
+        return false
     }
     func canSlideTileRight(atRow r : Int, Column c : Int) -> Bool {
-        return true
+        if (c < 3) {
+            if (state[r][c+1] == 0) {
+                return true
+            }
+        }
+        return false
     }
     func canSlideTile(atRow r : Int, Column c : Int) -> Bool {
-        return true
+        if (canSlideTileUp(atRow: r, Column: c) ||
+            canSlideTileDown(atRow: r, Column: c) ||
+            canSlideTileLeft(atRow: r, Column: c) ||
+            canSlideTileRight(atRow: r, Column: c) ) {
+            return true
+        }
+        return false
     }
     func slideTile(atRow r : Int, Column c: Int) {
-        
+        if canSlideTileUp(atRow: r, Column: c) {
+            state[r-1][c] = state[r][c]
+            state[r][c] = 0
+        } else if canSlideTileDown(atRow: r, Column: c) {
+            state[r+1][c] = state[r][c]
+            state[r][c] = 0
+        } else if canSlideTileLeft(atRow: r, Column: c) {
+            state[r][c-1] = state[r][c]
+            state[r][c] = 0
+        } else if canSlideTileRight(atRow: r, Column: c) {
+            state[r][c+1] = state[r][c]
+            state[r][c] = 0
+        }
     }
 }
